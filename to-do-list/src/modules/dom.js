8e046284f23df29b0a78da8740d568e6f3e3bb93
mahir <mahir21263@iiitd.ac.in>
2025-05-28 05:00:05 +0530

    feat:add project and add to do

--- to-do-list/src/modules/dom.js
+++ to-do-list/src/modules/dom.js
@@ -1,6 +1,32 @@
+import Project from "./project.js";
+
 function renderProject(project, container) {
     container.innerHTML = '';
+    const div_heading = document.createElement('div');
+    div_heading.classList.add("div-heading");
+    const heading = document.createElement('h1');
+    heading.textContent = `${project.name}`;
 
+    div_heading.appendChild(heading);
+    const addTodoBtn = document.createElement('button');
+    addTodoBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  </svg>
+  `;
+    addTodoBtn.classList.add('add-todo-btn');
+    div_heading.appendChild(addTodoBtn);
+    container.appendChild(div_heading);
+
+    addTodoBtn.addEventListener('click', () => {
+        const title = prompt('Enter title:');
+        const dueDate = prompt('Enter due date (YYYY-MM-DD):');
+        const priority = prompt('Enter priority (low/medium/high):');
+
+        if (!title) return;
+
+        project.addTodo(title, dueDate, priority);
+        renderProject(project, document.querySelector('.todo-main'));
+    });
     project.getTodos().forEach((todo) => {
         const todoDiv = document.createElement('div');
         todoDiv.classList.add('todo-item');
@@ -35,6 +61,7 @@ function renderProject(project, container) {
         });
         container.appendChild(todoDiv);
     });
+
 }
 
 function renderSidebar(projects, onSelectProject) {
@@ -50,6 +77,18 @@ function renderSidebar(projects, onSelectProject) {
         });
 
         sidebar.appendChild(projectBtn);
+
     });
+    const addProjectBtn = document.createElement('button');
+    addProjectBtn.textContent = '+ Add Project';
+    addProjectBtn.classList.add('add-project-btn');
+    addProjectBtn.addEventListener('click', () => {
+        const name = prompt('Enter project name');
+        if (!name) return;
+        const newProject = new Project(name);
+        projects.push(newProject);
+        renderSidebar(projects, onSelectProject);
+    });
+    sidebar.appendChild(addProjectBtn);
 }
 export { renderProject, renderSidebar };
