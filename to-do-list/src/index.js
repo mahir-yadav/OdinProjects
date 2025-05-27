ed42aa097cf7e0f02059d34b5bf4c9b8ca9bfeb6
mahir <mahir21263@iiitd.ac.in>
2025-05-28 05:11:56 +0530

    style:delete icon svg and default project

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -3,17 +3,12 @@ import { renderSidebar, renderProject } from './modules/dom.js';
 import Project from './modules/project.js';
 import Todo from './modules/todo.js';
 
-const inbox = new Project('Inbox');
-inbox.addTodo('Buy milk', '2025-06-01', 'high');
+const inbox = new Project('Default');
 inbox.addTodo('Do homework', '2025-06-03', 'medium');
 
 
 
-
-const work = new Project('Work');
-work.addTodo('Finish report', '2025-06-02', 'high');
-
-const projects = [inbox, work];
+const projects = [inbox];
 
 renderSidebar(projects, (selectedProject) => {
     renderProject(selectedProject, document.querySelector('.todo-main'));
