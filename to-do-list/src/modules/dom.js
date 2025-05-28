f8db0f6c8f0825c71e79a790972bb6ff621fa247
mahir <mahir21263@iiitd.ac.in>
2025-05-28 05:30:22 +0530

    feat:added edit button

--- to-do-list/src/modules/dom.js
+++ to-do-list/src/modules/dom.js
@@ -56,10 +56,31 @@ function renderProject(project, container) {
       `;
         delete_btn.classList.add('delete-btn');
         todoDiv.appendChild(delete_btn);
+
+        const edit_btn = document.createElement('button');
+        edit_btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg>`;
+        edit_btn.classList.add('edit-btn');
+        todoDiv.appendChild(edit_btn);
+
         delete_btn.addEventListener('click', () => {
             const index = project.getTodos().indexOf(todo);
             project.deleteToDo(index);
             renderProject(project, document.querySelector('.todo-main'));
+
+        });
+        edit_btn.addEventListener('click', () => {
+
+            const ntitle = prompt('Enter title:');
+            const ndueDate = prompt('Enter due date (YYYY-MM-DD):');
+            const npriority = prompt('Enter priority (low/medium/high):');
+
+            if (!title) return;
+            title.textContent = ntitle;
+            dueDate.textContent = ndueDate;
+            todoDiv.classList.remove(`${todo.priority}-priority`);
+            todoDiv.classList.add(`${npriority}-priority`);
+
 
         });
         container.appendChild(todoDiv);
